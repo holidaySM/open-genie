76b34319ac48def6afd6f4e10c2646fef3e52df1
sm <seungmin.lee@holiday-robotics.com>
2025-01-03 17:02:37 +0900

    feat: overwrite option for now

--- tokenizer.py
+++ tokenizer.py
@@ -3,17 +3,20 @@ from lightning.pytorch.cli import LightningCLI
 from genie import VideoTokenizer
 from genie.dataset import LightningPlatformer2D
 
+
 def cli_main():
-    '''
+    """
     Main function for the training script.
-    '''
-    
+    """
+
     # That's all it takes for LightningCLI to work!
     # No need to call .fit() or .test() or anything like that.
     cli = LightningCLI(
         VideoTokenizer,
         LightningPlatformer2D,
+        save_config_kwargs={"overwrite": True}
     )
 
-if __name__ == '__main__':    
+
+if __name__ == '__main__':
     cli_main()
